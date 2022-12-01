edf96bad8cb23663b5bfeff3fcf748ef4c4f8346
DayRaga <107507675+DayLucia@users.noreply.github.com>
2022-12-01 20:44:41 -0300

    prueba cansada ya

--- sw.js
+++ sw.js
@@ -1,13 +1,32 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('https://daylucia.github.io/dayanaragapwatp-02/sw.js').then(function(registration) {
-        // Si es exitoso
-        console.log('SW registrado correctamente');
-      })})}
-      else{
-        console.log('fallo al registrar SW')
-        // console.error();
-      };
-
-
 /*------------------------------------------CACHE E INSTALACIÓN------------------------------------------------*/
+
+const cacheName = 'cache-2';
+self.addEventListener('install', function (e){
+    console.log(e);
+    const cache = caches.open(cacheName).then( cache => {
+        return cache.addAll([
+          '/',
+          'index.html',
+          'app.js',
+          'icons/android-icon-72x72.png',
+          'css/style.css',
+          'sw.js',
+        ])
+    })
+    e.waitUntil( cache );
+})
+self.addEventListener( 'fetch', e =>{
+    const respuestaCache = caches.match(e.request).then( res => {
+        if ( res) {
+          return  res;
+        }
+        else {
+            return fetch ( e.request).then (respuesta =>{
+                caches.open(cacheName).then (cache =>{
+                    cache.put(e.request, respuesta)
+                })
+            })
+        }
+    })
+    e.respondWith(respuestaCache)
+})
