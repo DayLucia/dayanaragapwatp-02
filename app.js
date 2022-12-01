6630f083d4d1757ca91bf201d8e38a6901056bda
DayRaga <107507675+DayLucia@users.noreply.github.com>
2022-11-30 22:04:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+navigator.serviceWorker.register('sw.js');
+const btnSave = document.querySelector('#btn-save');
+const textArea = document.querySelector('#text-1');
+let container = document.querySelector('.collection');
+let lista = [];
+const cacheName = 'cache1';
+// FORMATO:
+//let lista = [ { nota: 'descripción de la nota', fecha: '17/11/2022'} ];
+
+//CACHE
+self.addEventListener('install', function(e){
+  console.log(e, 'instalando');
+  let cache = caches.open(cacheName).then(cache =>{
+    return cache.addAll([
+      'index.html',
+      'app.js',
+      'icon-72x72.png'
+    ])
+  })
+  e.waitUntil(cache);
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    let sideNav = document.querySelectorAll('.sidenav');
+    let instanciaSide = M.Sidenav.init(sideNav  , {});
+
+    let modal = document.querySelectorAll('.modal');
+    let instanciaModal = M.Modal.init(modal, {});
+
+    lista = leerNotas();
+    renderizarNotas(lista);
+});
+
+/* - FUNCION 1:  Obtiene el texto del textArea y guarda en el texto en el array - */
+btnSave.addEventListener('click', ()=>{
+  // //Obtengo el texto
+  // let texto = textArea.value;
+
+  // //Obtengo la fecha
+  // let tiempo = Date.now();
+  // let date = new Date(tiempo);
+  // let fecha = date.toLocaleDateString();
+
+  // //Verifico y creo la notita
+  // if(texto.length !== 0){
+  //   let nota = {
+  //     notita : texto,
+  //     fecha : fecha,
+  //   };
+  //   lista.push(nota);
+  // }
+  // guardarNotas(lista);
+})
+
+/* -------- FUNCION 2: Recibe el array y lo guarda en el localStorage ------- */
+function guardarNotas(lista){
+  
+  // localStorage.setItem('nota',JSON.stringify(lista));
+
+  renderizarNotas(lista);
+}
+
+/* --------- FUNCION 3: Lee los datos del localStorage y lo retorna --------- */
+function leerNotas(){
+  // let lista;
+  // if(localStorage.getItem('nota') !== false){
+  //   // lista = ;
+  //   lista = JSON.parse(localStorage.getItem('nota'));
+  //   console.log(lista, 'lista json')
+
+  //   // lista = [];
+  // } else{
+  //   lista = localStorage.getItem('nota');
+  //   // let nota = JSON.parse(lista);
+  //   // return nota;
+  // }
+}
+
+/* -------- FUNCION 4: Recibe el array y lo renderiza en el container ------- */
+function renderizarNotas(array){
+  // let html;
+  // if(array.length = 0){
+  //   html = `<h1>¡Ups, aun no añadiste notitas!</h1>`;
+  // }else{
+  //   array.forEach(dato => {
+  //     html = `
+  //             <p>${dato.fecha}</p>
+  //             <p>${dato.notita}</p>`
+  //   });
+
+  // }
+  
+}
